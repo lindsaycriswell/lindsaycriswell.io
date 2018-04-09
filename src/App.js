3712e8f9d68f72a036dacc11f3c857440f83398d
github username <github email address>
2018-04-08 22:39:58 -0700

    installed semantic

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App">LC MAKES STUFF</div>;
+    return (
+      <div className="App">
+        <div className="top-photo">LC MAKES STUFF</div>
+      </div>
+    );
   }
 }
 
 export default App;
+
+// <div style={{ height: "2000px" }}>
+//   <a href="#click">LC MAKES STUFF</a>
+// </div>
+// <div id="click" style={{ height: "2000px" }}>
+//   DID THIS WORK?
+// </div>
