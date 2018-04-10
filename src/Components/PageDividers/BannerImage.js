8c1294c98b13d01b8f56bb365a61c4a6d3220605
lindsaycriswell <lindsaycriswell@gmail.com>
2018-04-10 15:56:11 -0700

    working on banner image

---
+++ src/Components/PageDividers/BannerImage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Image } from "semantic-ui-react";
+import image from "./../../photos/top-photo.png";
+
+const BannerImage = () => {
+  return <Image src={image} fluid />;
+};
+
+export default BannerImage;
