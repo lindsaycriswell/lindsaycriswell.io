ca44222ac1cfa52c9c7e5fb25f437327497acffc
github username <github email address>
2018-04-10 16:28:07 -0700

    changed banner to background - need to syle

--- src/Components/PageDividers/BannerImage.js
+++ src/Components/PageDividers/BannerImage.js
@@ -1,8 +1,13 @@
 import React from "react";
 import image from "./../../photos/top-photo.png";
 
+// FIX BACKGROUND IMAGES
 const BannerImage = () => {
-  return <img src={image} className="page-divider" />;
+  return (
+    <div style={{ backgroundImage: `url(${image})`, height: "100vh" }}>
+      <h1>HI</h1>
+    </div>
+  );
 };
 
 export default BannerImage;
