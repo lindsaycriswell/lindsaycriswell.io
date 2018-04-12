e38ad30f7bb1d4066781cf9f6ff059c31f6393ae
github username <github email address>
2018-04-12 13:51:06 -0700

    top background image resized

--- src/Components/PageDividers/BannerImage.js
+++ src/Components/PageDividers/BannerImage.js
@@ -1,13 +1,7 @@
 import React from "react";
-import image from "./../../photos/top-photo.png";
 
-// FIX BACKGROUND IMAGES
 const BannerImage = () => {
-  return (
-    <div style={{ backgroundImage: `url(${image})`, height: "100vh" }}>
-      <h1>HI</h1>
-    </div>
-  );
+  return <div id="topImage" />;
 };
 
 export default BannerImage;
