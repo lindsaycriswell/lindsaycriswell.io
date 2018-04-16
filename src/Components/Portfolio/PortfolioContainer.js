47356126d6dc1f146979d3cb53d83974248069ae
lindsaycriswell <lindsaycriswell@gmail.com>
2018-04-15 17:50:48 -0700

    set up basic file structure

---
+++ src/Components/Portfolio/PortfolioContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PortfolioContainer = () => {
+  return (
+    <div>
+      <h1>Portfolio Container</h1>
+    </div>
+  );
+};
+
+export default PortfolioContainer;
