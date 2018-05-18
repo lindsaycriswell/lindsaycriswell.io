a176363ddaa250f6060768bd04a89322d2011605
lindsaycriswell <lindsaycriswell@gmail.com>
2018-05-18 14:10:19 -0400

    added portfolio data

--- src/Components/Portfolio/PortfolioContainer.js
+++ src/Components/Portfolio/PortfolioContainer.js
@@ -1,12 +1,20 @@
 import React from "react";
+import portfolioData from "./PortfolioData";
 
-const PortfolioContainer = () => {
-  return (
-    <div id="portfolio">
-      <h1>Portfolio Container</h1>
-    </div>
-  );
-};
+class PortfolioContainer extends React.Component {
+  state = {
+    index: 0
+  };
+
+  render() {
+    console.log(portfolioData[this.state.index]);
+    return (
+      <div id="portfolio">
+        <h1>Portfolio Container</h1>
+      </div>
+    );
+  }
+}
 
 export default PortfolioContainer;
 
