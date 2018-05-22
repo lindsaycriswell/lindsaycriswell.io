5f9aec4c1c7cca63b08e6f9d11dabbe3fb2766c1
lindsaycriswell <lindsaycriswell@gmail.com>
2018-05-22 18:39:07 -0400

    finished portfolio toggle

--- src/Components/Portfolio/PortfolioContainer.js
+++ src/Components/Portfolio/PortfolioContainer.js
@@ -8,10 +8,30 @@ class PortfolioContainer extends React.Component {
     index: 0
   };
 
+  handleArrowClick = increment => {
+    let newIndex = this.state.index + increment;
+    if (newIndex > 2) {
+      this.setState({
+        index: 0
+      });
+    } else if (newIndex < 0) {
+      this.setState({
+        index: 2
+      });
+    } else {
+      this.setState({
+        index: newIndex
+      });
+    }
+  };
+
   render() {
     return (
       <div id="portfolio">
-        <PortfolioImage project={portfolioData[this.state.index]} />
+        <PortfolioImage
+          project={portfolioData[this.state.index]}
+          handleArrowClick={this.handleArrowClick}
+        />
         <PortfolioDescription project={portfolioData[this.state.index]} />
       </div>
     );
